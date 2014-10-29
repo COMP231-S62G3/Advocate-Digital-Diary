673f66059840f1fdaa918c969b362fdfd9c2ccf4
parimalpatel <parimalpatel2@gmail.com>
2014-10-28 20:22:20 -0400

    User manual for case description.

--- AdvocateDigitalDiary/frmCaseType.Designer.cs
+++ AdvocateDigitalDiary/frmCaseType.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.sslabelCaseType = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtCaeTypeDescription = new System.Windows.Forms.TextBox();
             this.btnCaseTypeCancle = new System.Windows.Forms.Button();
             this.btnSaveCaseType = new System.Windows.Forms.Button();
-            this.txtCaeTypeDescription = new System.Windows.Forms.TextBox();
             this.lblCaseTypeDescription = new System.Windows.Forms.Label();
             this.txtCaseTypeName = new System.Windows.Forms.TextBox();
             this.lblCaseTypeName = new System.Windows.Forms.Label();
@@ -43,84 +43,92 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.sslabelCaseType});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 393);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(459, 22);
-            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(612, 25);
+            this.statusStrip1.TabIndex = 18;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // sslabelCaseType
             // 
             this.sslabelCaseType.Name = "sslabelCaseType";
-            this.sslabelCaseType.Size = new System.Drawing.Size(16, 17);
+            this.sslabelCaseType.Size = new System.Drawing.Size(18, 20);
             this.sslabelCaseType.Text = "...";
+            // 
+            // txtCaeTypeDescription
+            // 
+            this.txtCaeTypeDescription.Location = new System.Drawing.Point(96, 37);
+            this.txtCaeTypeDescription.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCaeTypeDescription.MaxLength = 100;
+            this.txtCaeTypeDescription.Multiline = true;
+            this.txtCaeTypeDescription.Name = "txtCaeTypeDescription";
+            this.txtCaeTypeDescription.Size = new System.Drawing.Size(383, 198);
+            this.txtCaeTypeDescription.TabIndex = 14;
             // 
             // btnCaseTypeCancle
             // 
             this.btnCaseTypeCancle.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCaseTypeCancle.Location = new System.Drawing.Point(285, 213);
+            this.btnCaseTypeCancle.Location = new System.Drawing.Point(380, 257);
+            this.btnCaseTypeCancle.Margin = new System.Windows.Forms.Padding(4);
             this.btnCaseTypeCancle.Name = "btnCaseTypeCancle";
-            this.btnCaseTypeCancle.Size = new System.Drawing.Size(75, 23);
-            this.btnCaseTypeCancle.TabIndex = 10;
+            this.btnCaseTypeCancle.Size = new System.Drawing.Size(100, 28);
+            this.btnCaseTypeCancle.TabIndex = 17;
             this.btnCaseTypeCancle.Text = "Close";
             this.btnCaseTypeCancle.UseVisualStyleBackColor = true;
             // 
             // btnSaveCaseType
             // 
-            this.btnSaveCaseType.Location = new System.Drawing.Point(204, 213);
+            this.btnSaveCaseType.Location = new System.Drawing.Point(272, 257);
+            this.btnSaveCaseType.Margin = new System.Windows.Forms.Padding(4);
             this.btnSaveCaseType.Name = "btnSaveCaseType";
-            this.btnSaveCaseType.Size = new System.Drawing.Size(75, 23);
-            this.btnSaveCaseType.TabIndex = 8;
+            this.btnSaveCaseType.Size = new System.Drawing.Size(100, 28);
+            this.btnSaveCaseType.TabIndex = 15;
             this.btnSaveCaseType.Text = "Save";
             this.btnSaveCaseType.UseVisualStyleBackColor = true;
-            // 
-            // txtCaeTypeDescription
-            // 
-            this.txtCaeTypeDescription.Location = new System.Drawing.Point(72, 34);
-            this.txtCaeTypeDescription.MaxLength = 100;
-            this.txtCaeTypeDescription.Multiline = true;
-            this.txtCaeTypeDescription.Name = "txtCaeTypeDescription";
-            this.txtCaeTypeDescription.Size = new System.Drawing.Size(288, 162);
-            this.txtCaeTypeDescription.TabIndex = 7;
             // 
             // lblCaseTypeDescription
             // 
             this.lblCaseTypeDescription.AutoSize = true;
-            this.lblCaseTypeDescription.Location = new System.Drawing.Point(6, 37);
+            this.lblCaseTypeDescription.Location = new System.Drawing.Point(8, 41);
+            this.lblCaseTypeDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCaseTypeDescription.Name = "lblCaseTypeDescription";
-            this.lblCaseTypeDescription.Size = new System.Drawing.Size(60, 13);
-            this.lblCaseTypeDescription.TabIndex = 9;
+            this.lblCaseTypeDescription.Size = new System.Drawing.Size(79, 17);
+            this.lblCaseTypeDescription.TabIndex = 16;
             this.lblCaseTypeDescription.Text = "Description";
             // 
             // txtCaseTypeName
             // 
-            this.txtCaseTypeName.Location = new System.Drawing.Point(72, 8);
+            this.txtCaseTypeName.Location = new System.Drawing.Point(96, 5);
+            this.txtCaseTypeName.Margin = new System.Windows.Forms.Padding(4);
             this.txtCaseTypeName.MaxLength = 30;
             this.txtCaseTypeName.Name = "txtCaseTypeName";
-            this.txtCaseTypeName.Size = new System.Drawing.Size(288, 20);
-            this.txtCaseTypeName.TabIndex = 5;
+            this.txtCaseTypeName.Size = new System.Drawing.Size(383, 22);
+            this.txtCaseTypeName.TabIndex = 12;
             // 
             // lblCaseTypeName
             // 
             this.lblCaseTypeName.AutoSize = true;
-            this.lblCaseTypeName.Location = new System.Drawing.Point(31, 11);
+            this.lblCaseTypeName.Location = new System.Drawing.Point(41, 9);
+            this.lblCaseTypeName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCaseTypeName.Name = "lblCaseTypeName";
-            this.lblCaseTypeName.Size = new System.Drawing.Size(35, 13);
-            this.lblCaseTypeName.TabIndex = 6;
+            this.lblCaseTypeName.Size = new System.Drawing.Size(45, 17);
+            this.lblCaseTypeName.TabIndex = 13;
             this.lblCaseTypeName.Text = "Name";
             // 
             // frmCaseType
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(459, 415);
+            this.ClientSize = new System.Drawing.Size(612, 511);
             this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtCaeTypeDescription);
             this.Controls.Add(this.btnCaseTypeCancle);
             this.Controls.Add(this.btnSaveCaseType);
-            this.Controls.Add(this.txtCaeTypeDescription);
             this.Controls.Add(this.lblCaseTypeDescription);
             this.Controls.Add(this.txtCaseTypeName);
             this.Controls.Add(this.lblCaseTypeName);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmCaseType";
             this.Text = "Case Type";
             this.statusStrip1.ResumeLayout(false);
@@ -134,11 +142,13 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel sslabelCaseType;
+        private System.Windows.Forms.TextBox txtCaeTypeDescription;
         private System.Windows.Forms.Button btnCaseTypeCancle;
         private System.Windows.Forms.Button btnSaveCaseType;
-        private System.Windows.Forms.TextBox txtCaeTypeDescription;
         private System.Windows.Forms.Label lblCaseTypeDescription;
         private System.Windows.Forms.TextBox txtCaseTypeName;
         private System.Windows.Forms.Label lblCaseTypeName;
+
+
     }
 }
--- AdvocateDigitalDiary/frmCaseTypesList.Designer.cs
+++ AdvocateDigitalDiary/frmCaseTypesList.Designer.cs
@@ -30,102 +30,91 @@
         {
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.sslabelCaseType = new System.Windows.Forms.ToolStripStatusLabel();
-            this.btnCaseTypeCancle = new System.Windows.Forms.Button();
-            this.btnSaveCaseType = new System.Windows.Forms.Button();
-            this.txtCaeTypeDescription = new System.Windows.Forms.TextBox();
-            this.lblCaseTypeDescription = new System.Windows.Forms.Label();
-            this.txtCaseTypeName = new System.Windows.Forms.TextBox();
-            this.lblCaseTypeName = new System.Windows.Forms.Label();
+            this.btnShowCaseTypeClose = new System.Windows.Forms.Button();
+            this.btnCaseTypeDelete = new System.Windows.Forms.Button();
+            this.btnCaseTypeEdit = new System.Windows.Forms.Button();
+            this.dgvCaseType = new System.Windows.Forms.DataGridView();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCaseType)).BeginInit();
             this.SuspendLayout();
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.sslabelCaseType});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 438);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(463, 22);
-            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(617, 25);
+            this.statusStrip1.TabIndex = 9;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // sslabelCaseType
             // 
             this.sslabelCaseType.Name = "sslabelCaseType";
-            this.sslabelCaseType.Size = new System.Drawing.Size(16, 17);
+            this.sslabelCaseType.Size = new System.Drawing.Size(18, 20);
             this.sslabelCaseType.Text = "...";
             // 
-            // btnCaseTypeCancle
+            // btnShowCaseTypeClose
             // 
-            this.btnCaseTypeCancle.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCaseTypeCancle.Location = new System.Drawing.Point(285, 214);
-            this.btnCaseTypeCancle.Name = "btnCaseTypeCancle";
-            this.btnCaseTypeCancle.Size = new System.Drawing.Size(75, 23);
-            this.btnCaseTypeCancle.TabIndex = 10;
-            this.btnCaseTypeCancle.Text = "Close";
-            this.btnCaseTypeCancle.UseVisualStyleBackColor = true;
+            this.btnShowCaseTypeClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnShowCaseTypeClose.Location = new System.Drawing.Point(399, 273);
+            this.btnShowCaseTypeClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShowCaseTypeClose.Name = "btnShowCaseTypeClose";
+            this.btnShowCaseTypeClose.Size = new System.Drawing.Size(100, 28);
+            this.btnShowCaseTypeClose.TabIndex = 8;
+            this.btnShowCaseTypeClose.Text = "Close";
+            this.btnShowCaseTypeClose.UseVisualStyleBackColor = true;
             // 
-            // btnSaveCaseType
+            // btnCaseTypeDelete
             // 
-            this.btnSaveCaseType.Location = new System.Drawing.Point(204, 214);
-            this.btnSaveCaseType.Name = "btnSaveCaseType";
-            this.btnSaveCaseType.Size = new System.Drawing.Size(75, 23);
-            this.btnSaveCaseType.TabIndex = 8;
-            this.btnSaveCaseType.Text = "Save";
-            this.btnSaveCaseType.UseVisualStyleBackColor = true;
+            this.btnCaseTypeDelete.Location = new System.Drawing.Point(288, 273);
+            this.btnCaseTypeDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCaseTypeDelete.Name = "btnCaseTypeDelete";
+            this.btnCaseTypeDelete.Size = new System.Drawing.Size(103, 28);
+            this.btnCaseTypeDelete.TabIndex = 7;
+            this.btnCaseTypeDelete.Text = "Delete";
+            this.btnCaseTypeDelete.UseVisualStyleBackColor = true;
             // 
-            // txtCaeTypeDescription
+            // btnCaseTypeEdit
             // 
-            this.txtCaeTypeDescription.Location = new System.Drawing.Point(72, 35);
-            this.txtCaeTypeDescription.MaxLength = 100;
-            this.txtCaeTypeDescription.Multiline = true;
-            this.txtCaeTypeDescription.Name = "txtCaeTypeDescription";
-            this.txtCaeTypeDescription.Size = new System.Drawing.Size(288, 162);
-            this.txtCaeTypeDescription.TabIndex = 7;
+            this.btnCaseTypeEdit.Location = new System.Drawing.Point(180, 273);
+            this.btnCaseTypeEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCaseTypeEdit.Name = "btnCaseTypeEdit";
+            this.btnCaseTypeEdit.Size = new System.Drawing.Size(100, 28);
+            this.btnCaseTypeEdit.TabIndex = 6;
+            this.btnCaseTypeEdit.Text = "Edit";
+            this.btnCaseTypeEdit.UseVisualStyleBackColor = true;
             // 
-            // lblCaseTypeDescription
+            // dgvCaseType
             // 
-            this.lblCaseTypeDescription.AutoSize = true;
-            this.lblCaseTypeDescription.Location = new System.Drawing.Point(6, 38);
-            this.lblCaseTypeDescription.Name = "lblCaseTypeDescription";
-            this.lblCaseTypeDescription.Size = new System.Drawing.Size(60, 13);
-            this.lblCaseTypeDescription.TabIndex = 9;
-            this.lblCaseTypeDescription.Text = "Description";
-            // 
-            // txtCaseTypeName
-            // 
-            this.txtCaseTypeName.Location = new System.Drawing.Point(72, 9);
-            this.txtCaseTypeName.MaxLength = 30;
-            this.txtCaseTypeName.Name = "txtCaseTypeName";
-            this.txtCaseTypeName.Size = new System.Drawing.Size(288, 20);
-            this.txtCaseTypeName.TabIndex = 5;
-            // 
-            // lblCaseTypeName
-            // 
-            this.lblCaseTypeName.AutoSize = true;
-            this.lblCaseTypeName.Location = new System.Drawing.Point(31, 12);
-            this.lblCaseTypeName.Name = "lblCaseTypeName";
-            this.lblCaseTypeName.Size = new System.Drawing.Size(35, 13);
-            this.lblCaseTypeName.TabIndex = 6;
-            this.lblCaseTypeName.Text = "Name";
+            this.dgvCaseType.AllowUserToAddRows = false;
+            this.dgvCaseType.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCaseType.Location = new System.Drawing.Point(13, 7);
+            this.dgvCaseType.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvCaseType.MultiSelect = false;
+            this.dgvCaseType.Name = "dgvCaseType";
+            this.dgvCaseType.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCaseType.Size = new System.Drawing.Size(484, 246);
+            this.dgvCaseType.TabIndex = 5;
             // 
             // frmCaseTypesList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 376);
+            this.ClientSize = new System.Drawing.Size(617, 463);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.btnCaseTypeCancle);
-            this.Controls.Add(this.btnSaveCaseType);
-            this.Controls.Add(this.txtCaeTypeDescription);
-            this.Controls.Add(this.lblCaseTypeDescription);
-            this.Controls.Add(this.txtCaseTypeName);
-            this.Controls.Add(this.lblCaseTypeName);
+            this.Controls.Add(this.btnShowCaseTypeClose);
+            this.Controls.Add(this.btnCaseTypeDelete);
+            this.Controls.Add(this.btnCaseTypeEdit);
+            this.Controls.Add(this.dgvCaseType);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmCaseTypesList";
             this.Text = "Case Type";
             this.Load += new System.EventHandler(this.frmCaseTypesList_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCaseType)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,11 +124,10 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel sslabelCaseType;
-        private System.Windows.Forms.Button btnCaseTypeCancle;
-        private System.Windows.Forms.Button btnSaveCaseType;
-        private System.Windows.Forms.TextBox txtCaeTypeDescription;
-        private System.Windows.Forms.Label lblCaseTypeDescription;
-        private System.Windows.Forms.TextBox txtCaseTypeName;
-        private System.Windows.Forms.Label lblCaseTypeName;
+        private System.Windows.Forms.Button btnShowCaseTypeClose;
+        private System.Windows.Forms.Button btnCaseTypeDelete;
+        private System.Windows.Forms.Button btnCaseTypeEdit;
+        private System.Windows.Forms.DataGridView dgvCaseType;
+
     }
 }
