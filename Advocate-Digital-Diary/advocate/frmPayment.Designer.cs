45be2843cc030ab8660e2397a8577b517bfd57dd
parimalpatel <parimalpatel2@gmail.com>
2014-11-14 12:14:45 -0500

    Design of Payment Page

---
+++ Advocate-Digital-Diary/advocate/frmPayment.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace AdvocateDigitalDiary
+{
+    partial class frmPayment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sslabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dgvPayment = new System.Windows.Forms.DataGridView();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtBank = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtParticular = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboMode = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPayment)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sslabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 393);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(633, 25);
+            this.statusStrip1.TabIndex = 29;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // sslabel
+            // 
+            this.sslabel.Name = "sslabel";
+            this.sslabel.Size = new System.Drawing.Size(18, 20);
+            this.sslabel.Text = "...";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(489, 342);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 28);
+            this.btnClose.TabIndex = 28;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dgvPayment
+            // 
+            this.dgvPayment.AllowUserToAddRows = false;
+            this.dgvPayment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPayment.Location = new System.Drawing.Point(13, 115);
+            this.dgvPayment.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvPayment.MultiSelect = false;
+            this.dgvPayment.Name = "dgvPayment";
+            this.dgvPayment.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPayment.Size = new System.Drawing.Size(600, 219);
+            this.dgvPayment.TabIndex = 27;
+            this.dgvPayment.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPayment_CellContentClick);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(490, 72);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 28);
+            this.btnRemove.TabIndex = 26;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(382, 72);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 25;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtBank
+            // 
+            this.txtBank.Location = new System.Drawing.Point(89, 72);
+            this.txtBank.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBank.MaxLength = 20;
+            this.txtBank.Name = "txtBank";
+            this.txtBank.Size = new System.Drawing.Size(212, 22);
+            this.txtBank.TabIndex = 24;
+            this.txtBank.TextChanged += new System.EventHandler(this.txtBank_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 76);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Bank";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtParticular
+            // 
+            this.txtParticular.Location = new System.Drawing.Point(90, 42);
+            this.txtParticular.Margin = new System.Windows.Forms.Padding(4);
+            this.txtParticular.MaxLength = 20;
+            this.txtParticular.Name = "txtParticular";
+            this.txtParticular.Size = new System.Drawing.Size(211, 22);
+            this.txtParticular.TabIndex = 22;
+            this.txtParticular.TextChanged += new System.EventHandler(this.txtParticular_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-3, 44);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Particular No.";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cboMode
+            // 
+            this.cboMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMode.FormattingEnabled = true;
+            this.cboMode.Items.AddRange(new object[] {
+            "Cash",
+            "Cheque",
+            "Demand Draft"});
+            this.cboMode.Location = new System.Drawing.Point(90, 7);
+            this.cboMode.Margin = new System.Windows.Forms.Padding(4);
+            this.cboMode.Name = "cboMode";
+            this.cboMode.Size = new System.Drawing.Size(211, 24);
+            this.cboMode.TabIndex = 20;
+            this.cboMode.SelectedIndexChanged += new System.EventHandler(this.cboMode_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Mode";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(382, 40);
+            this.txtAmount.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAmount.MaxLength = 10;
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(211, 22);
+            this.txtAmount.TabIndex = 18;
+            this.txtAmount.TextChanged += new System.EventHandler(this.txtAmount_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Amount";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(382, 8);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(211, 22);
+            this.dateTimePicker1.TabIndex = 16;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Date";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmPayment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 418);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvPayment);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtBank);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtParticular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cboMode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPayment";
+            this.Text = "Payment";
+            this.Load += new System.EventHandler(this.frmPayment_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPayment)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sslabel;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.DataGridView dgvPayment;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtBank;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtParticular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboMode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Advocate-Digital-Diary/advocate/frmPayment.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdvocateDigitalDiary
+{
+    public partial class frmPayment : Form
+    {
+        public frmPayment()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPayment_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtParticular_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBank_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPayment_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
