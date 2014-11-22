d4ba5003fe40df45796b4cfe8fc0e7d6f2e2c94f
Nazneen01 <nazneenshibly@gmail.com>
2014-11-22 02:06:30 -0500

    Design of Documents Page

---
+++ Advocate-Digital-Diary/advocate/frmDocument.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace advocate
+{
+    partial class frmDocument
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtDocument = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.dgvDocument = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sslabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocument)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(114, 16);
+            this.txtName.MaxLength = 20;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(217, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // dtDocument
+            // 
+            this.dtDocument.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtDocument.Location = new System.Drawing.Point(114, 42);
+            this.dtDocument.Name = "dtDocument";
+            this.dtDocument.Size = new System.Drawing.Size(217, 20);
+            this.dtDocument.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Receiving Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(114, 68);
+            this.txtDescription.MaxLength = 100;
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(217, 78);
+            this.txtDescription.TabIndex = 5;
+            // 
+            // dgvDocument
+            // 
+            this.dgvDocument.AllowUserToAddRows = false;
+            this.dgvDocument.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocument.Location = new System.Drawing.Point(12, 181);
+            this.dgvDocument.MultiSelect = false;
+            this.dgvDocument.Name = "dgvDocument";
+            this.dgvDocument.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDocument.Size = new System.Drawing.Size(319, 150);
+            this.dgvDocument.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(175, 152);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(256, 152);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(256, 337);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sslabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 365);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(345, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sslabel
+            // 
+            this.sslabel.Name = "sslabel";
+            this.sslabel.Size = new System.Drawing.Size(16, 17);
+            this.sslabel.Text = "...";
+            // 
+            // frmDocument
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 387);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvDocument);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtDocument);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDocument";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Documents";
+            this.Load += new System.EventHandler(this.frmDocument_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocument)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker dtDocument;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.DataGridView dgvDocument;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sslabel;
+    }
+}
---
+++ Advocate-Digital-Diary/advocate/frmDocument.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace advocate
+{
+    public partial class frmDocument : Form
+    {
+        int caseId;
+        BllDocument obj = new BllDocument();
+        public void StartDocument(int value)
+        {
+            caseId = value;
+            this.ShowDialog();
+        }
+        public frmDocument()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDocument_Load(object sender, EventArgs e)
+        {
+            BllDocument obj2 = new BllDocument();
+            DataTable tb = obj2.GetDocuments(caseId);
+            dgvDocument.DataSource = tb;
+            dgvDocument.Columns[0].Visible = false;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool ValidateData()
+        {
+            if (txtName.Text.Length == 0)
+            {
+                sslabel.ForeColor = Color.Red;
+                sslabel.Text = " Please enter a valid Name...";
+                return (false);
+            }
+           
+            return (true);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bool check1 = ValidateData();
+            if (check1 == true)
+            {
+                obj.Name = txtName.Text;
+                obj.ReceivingDate = dtDocument.Value;
+                obj.Description = txtDescription.Text;
+                obj.CaseId = caseId;
+                obj.SaveDocument();
+                sslabel.ForeColor = Color.Black;
+                sslabel.Text = "New Document Added Successfully....";
+
+                txtDescription.Clear();
+                txtName.Clear();
+
+                BllDocument obj2 = new BllDocument();
+                DataTable tb = obj2.GetDocuments(caseId);
+                dgvDocument.DataSource = tb;
+                dgvDocument.Columns[0].Visible = false;
+            }
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dgvDocument.SelectedRows.Count > 0)
+            {
+                DialogResult dr;
+                dr = MessageBox.Show("Do You Want To Delete it?", "Delete?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+
+                    obj.DocumentNo = Convert.ToInt32(dgvDocument.SelectedRows[0].Cells[0].Value);
+                    obj.DeleteDocument();
+                    sslabel.ForeColor = Color.Black;
+                    sslabel.Text = "Document Record Successfully Removed...";
+
+                    BllDocument obj2 = new BllDocument();
+                    DataTable tb = obj2.GetDocuments(caseId);
+                    dgvDocument.DataSource = tb;
+                    dgvDocument.Columns[0].Visible = false;
+                }
+
+            }
+            else
+            {
+                sslabel.ForeColor = Color.Red;
+                sslabel.Text = "Please select an item...";
+            }
+        }
+    }
+}
