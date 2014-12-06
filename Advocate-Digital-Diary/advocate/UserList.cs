4ac7c6574f47ba0910074c413fb9e585afde234d
Nazneen01 <nazneenshibly@gmail.com>
2014-12-06 01:25:02 -0500

    Coding of User List Page

--- Advocate-Digital-Diary/advocate/UserList.cs
+++ Advocate-Digital-Diary/advocate/UserList.cs
@@ -18,23 +18,53 @@ namespace advocate
 
         private void UserList_Load(object sender, EventArgs e)
         {
-            
+            BLLUser obj = new BLLUser();
+            DataTable tb = obj.GetAllusers();
+            dgvUsers.DataSource = tb;
+            dgvUsers.Columns[0].Visible = false;
+            dgvUsers.Columns[5].Visible = false;
+
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            
+            DialogResult dr = MessageBox.Show("Do you really want to delete it??","Delete?",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                if (dgvUsers.SelectedRows.Count > 0)
+                {
+                    int a = Convert.ToInt32(dgvUsers.SelectedRows[0].Cells[0].Value);
+                    BLLUser obj = new BLLUser();
+                    obj.DeleteUser(a);
+                    DataTable tb = obj.GetAllusers();
+                    dgvUsers.DataSource = tb;
+                    dgvUsers.Columns[0].Visible = false;
+                    dgvUsers.Columns[5].Visible = false;
+
+                    ssLabel.Text = "Deleted Successfully..";
+                }
+            }
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            
+            if (dgvUsers.SelectedRows.Count > 0)
+            {
+                FrmUser obj = new FrmUser();
+                int a = Convert.ToInt32(dgvUsers.SelectedRows[0].Cells[0].Value);
+                obj.StartUser(FrmUser.eTranType.Edit, a);
+
+                BLLUser BLLobj = new BLLUser();
+                DataTable tb = BLLobj.GetAllusers();
+                dgvUsers.DataSource = tb;
+                dgvUsers.Columns[0].Visible = false;
+                dgvUsers.Columns[5].Visible = false;
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
-            
+            this.Close();
         }
     }
 }
-
